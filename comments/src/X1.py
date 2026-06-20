91a5c806293609d3ed5ed19d24d68b28c82728b5
Padraic Harley <padraic@thelodgeronline.com>
2026-06-20 10:29:35 +0100

    It works!

--- comments/src/X1.py
+++ comments/src/X1.py
@@ -4,15 +4,11 @@ class X1:
     def sum_of_squares(lower_bound, upper_bound):
         total = 0
 
-        # Iterate from lower bound (q) to upper bound (z)
         for num in range(lower_bound, upper_bound + 1):
-            # Add square of each number in the range
             total += X1.square(num)
 
-        # Return accumulated sum
         return total
 
     @staticmethod
     def square(num):
-        # Return square of input
         return num * num
