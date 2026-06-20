11f811e71a3192a9f6928b9badd54f5d949134d0
Padraic Harley <padraic@thelodgeronline.com>
2026-06-20 10:22:54 +0100

    It works!

--- comments/src/X1.py
+++ comments/src/X1.py
@@ -2,15 +2,15 @@ class X1:
 
     @staticmethod
     def sum_of_squares(lower_bound, upper_bound):
-        p = 0
+        total = 0
 
         # Iterate from lower bound (q) to upper bound (z)
         for i in range(lower_bound, upper_bound + 1):
             # Add square of each number in the range
-            p += X1.square(i)
+            total += X1.square(i)
 
         # Return accumulated sum
-        return p
+        return total
 
     @staticmethod
     def square(k):
