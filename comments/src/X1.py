903bc09cd43388e61ffa47cbc17f2f5ede285110
Padraic Harley <padraic@thelodgeronline.com>
2026-06-20 10:22:19 +0100

    It works!

--- comments/src/X1.py
+++ comments/src/X1.py
@@ -1,11 +1,11 @@
 class X1:
 
     @staticmethod
-    def sum_of_squares(q, z):
+    def sum_of_squares(lower_bound, upper_bound):
         p = 0
 
         # Iterate from lower bound (q) to upper bound (z)
-        for i in range(q, z + 1):
+        for i in range(lower_bound, upper_bound + 1):
             # Add square of each number in the range
             p += X1.square(i)
 
